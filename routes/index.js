7ec540e0914fd627508e1af946edac41a728c049
Nicole Zhu <nicolez@stanford.edu>
2014-02-05 10:57:55 -0800

    fin till slide 21, can get data from JSON file

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,16 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	res.render('index', data);
 };
+
+
+
+// exports.view = function(req, res){
+//   res.render('index', {
+//     'name': 'Waiting in Line',
+//     'image': 'lorempixel.people.1.jpeg',
+//     'id': 'project1'
+//   });
+// };
+
