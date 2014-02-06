1498fa8e3cd577801407f865580528f4fe7b8488
Nicole Zhu <nicolez@stanford.edu>
2014-02-05 16:04:34 -0800

    done!

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,29 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	var name = req.query.name;
+	var description = req.query.description;
+    var imageURL = 'http://lorempixel.com/400/400/people'
+	console.log(name);
+	console.log(description);
+	console.log(imageURL);
+
+	// res.render('add', {
+	// 	'friends': [
+	// 		{
+	// 		'imageURL': imageURL,
+	// 		'name': name,
+	// 		'description': description
+	// 		}
+	// 	]
+	// });
+
+	var newFriend = {
+		name: name,
+		description: description,
+		imageURL: imageURL
+	};
+	data['friends'].push(newFriend);
+
+	res.render('add', data);
  }
